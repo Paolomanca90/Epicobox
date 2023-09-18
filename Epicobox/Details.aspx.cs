39b287b9cdd8d13a0e5906e114a4b88da587fade
Erica Diana <134380180+EricaDianaa@users.noreply.github.com>
2023-09-18 16:46:24 +0200

    dettagli

--- Epicobox/Details.aspx.cs
+++ Epicobox/Details.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,40 @@ namespace Epicobox
 {
     public partial class Details : System.Web.UI.Page
     {
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+                string connection = ConfigurationManager.ConnectionStrings["ConnectionDB"]
+                .ConnectionString.ToString();
+
+                SqlConnection conn = new SqlConnection(connection);
+                SqlCommand cmd = new SqlCommand("select * from Esperienze where IdEsperienza=@id", conn);
+                cmd.Parameters.AddWithValue("id", Request.QueryString["IdProdotto"]);
+                conn.Open();
+                SqlDataReader sqlreader;
+                sqlreader = cmd.ExecuteReader();
+                while (sqlreader.Read())
+                {
+                    Nome.InnerHtml = $"{sqlreader["Nome"]}";
+                    Prezzo.InnerHtml = $"{sqlreader["Prezzo"]}";
+                    DescrizioneLunga.InnerHtml = $"{sqlreader["DescrizioneLunga"]}";
+                    dataInizio.InnerHtml = $"{sqlreader["DataInizio"]}";
+                    dataFine.InnerHtml = $"{sqlreader["DataFine"]}";
+                    location.InnerHtml = $"{sqlreader["Location"]}";
+                    categoria.InnerHtml = $"{sqlreader["Categoria"]}";
+                    imageBox.ImageUrl = $"{sqlreader["ImageBox"]}";
+                    image1.ImageUrl = $"{sqlreader["ImageBox"]}";
+                    image2.ImageUrl = $"{sqlreader["ImageBox"]}";
+                    image3.ImageUrl = $"{sqlreader["ImageBox"]}";
+            }
+
+            conn.Close();
+            
+
+
+
 
         }
     }
